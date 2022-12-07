af4b872fd2f2416d511d8ae0d96a6f3620875b91
angelina-tsuboi <angelina.t1832@gmail.com>
2022-12-07 01:11:24 -0800

    fix graphics display

--- GiveAStar.ino
+++ GiveAStar.ino
@@ -1,9 +1,10 @@
 #include "SH1106Wire.h"
+#include "graphics.h"
 
 SH1106Wire display(0x3C, 33, 35);
 
 const int leds[5] = {3, 5, 7, 9, 11};
-const string text[4] = {"Hello World", "You are Really Awesome", "Jshddsj dsahjdaskhjd dashdasjkhdjas dashdasjkhdsaj adshdaskjhdsaj daskjadshjkdas"};
+const String text[4] = {"Hello World", "You are Really Awesome", "Jshddsj dsahjdaskhjd dashdasjkhdjas dashdasjkhdsaj adshdaskjhdsaj daskjadshjkdas"};
 const int button = 16;
 // 0 = no animation, 
 int starAnimation = 0;
@@ -17,8 +18,7 @@ void setup() {
   display.flipScreenVertically();
   display.setTextAlignment(TEXT_ALIGN_LEFT);
   display.setFont(ArialMT_Plain_10);
-  display.drawString(18, 15, "Project Give A Star!");
-  display.drawString(20, 35, "By Angelina Tsuboi");
+  display.drawXbm(0, 5, logo_width, logo_height, logo);
   display.display();
 
   // LED Setup
@@ -26,8 +26,6 @@ void setup() {
     pinMode(leds[i], OUTPUT);
   }
   pinMode(button, INPUT);
-
-  display.drawXbm(50, 5, logo_width, logo_height, logo);
 }
 
 void loop() {
