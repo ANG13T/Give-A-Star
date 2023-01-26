af5ab9c6c715e342708951b01c4b2db214cf038f
angelina-tsuboi <angelina.t1832@gmail.com>
2023-01-25 18:15:19 -0800

    feat: commented code

--- GiveAStar/GiveAStar.ino
+++ GiveAStar/GiveAStar.ino
@@ -1,27 +1,25 @@
 #include "SH1106Wire.h"
 #include "graphics.h"
 
-// Fix Software at End, increase delays, make text display better, add images to README, make 3 min video for YouTube, publish
-
 SH1106Wire display(0x3C, 33, 35);
-
 const int leds[5] = {5, 7, 9, 11, 12};
 const int button = 16;
-// 0 = no animation, 1 = in order, 2 = pairs (set starting point), 3 = flashing
+
+// 0 = no animation, 1 = in order sequence, 2 = pairs (set starting point), 3 = flashing
 int starAnimation = 0;
 
-// Indeces for each animation
-int selectedLED2 = 0;
+// variables for animation controls
+int selectedLED = 0;
 bool isFlashing = true;
-int selectedSlide = 0;
 
-long interval = 1000;
+// timing controls
+long interval = 1000; // length of animation interval
 long previousMillis = 0;
 
 void setup() {
   Serial.begin(115200);
 
-  // OLED Setup
+  // OLED set up with logo
   display.init();
   display.flipScreenVertically();
   display.setTextAlignment(TEXT_ALIGN_LEFT);
@@ -30,10 +28,11 @@ void setup() {
   display.display();
   display.setTextAlignment(TEXT_ALIGN_CENTER);
 
-  // LED Setup
+  // LED set up
   for (int i = 0; i < 5; i++) {
     pinMode(leds[i], OUTPUT);
   }
+  
   pinMode(button, INPUT);
   delay(1000);
   display.clear();
@@ -41,51 +40,58 @@ void setup() {
 
 void loop() {
   int pushed = digitalRead(button);
+  unsigned long currentMillis = millis();
 
+  // button input code (sets animation number)
   if (pushed == HIGH) {
-    Serial.print("inside");
+
     if (starAnimation == 3) {
       starAnimation = 0;
     } else  {
       starAnimation += 1;
       if (starAnimation == 2) {
-        selectedLED2 = 0;
+        selectedLED = 0;
       }
     }
+    
     resetLEDs();
     delay(500);
   }
-
-  unsigned long currentMillis = millis();
 
   if (currentMillis - previousMillis > interval) {
     // save the last time you blinked the LED
     previousMillis = currentMillis;
 
     if (starAnimation == 0) {
+      // Animation 1: All LEDs on
       showStarImage(0);
       for (int i = 0; i < 5; i++) {
         digitalWrite(leds[i], HIGH);
       }
+      
     } else if (starAnimation == 1) {
+      // Animation 2: One LED at a time sequence
       showStarImage(1);
-      digitalWrite(leds[selectedLED2], HIGH);
-      digitalWrite(leds[getPrevLED(selectedLED2)], LOW);
-      selectedLED2 = getNextLED(selectedLED2);
+      digitalWrite(leds[selectedLED], HIGH);
+      digitalWrite(leds[getPrevLED(selectedLED)], LOW);
+      selectedLED = getNextLED(selectedLED);
+      
     } else if (starAnimation == 2) {
+       // Animation 3: LED pairs animation
       showStarImage(2);
-      resetLEDs();
-      if (selectedLED2 == 0) {
-        digitalWrite(leds[2], HIGH);
-      } else if (selectedLED2 == 1) {
-        digitalWrite(leds[1], HIGH);
+      if (selectedLED == 0) {
+        digitalWrite(leds[2], HIGH); // top led
+      } else if (selectedLED == 1) {
+        digitalWrite(leds[1], HIGH); // middle leds
         digitalWrite(leds[3], HIGH);
       } else {
-        digitalWrite(leds[0], HIGH);
+        digitalWrite(leds[0], HIGH); // bottom leds
         digitalWrite(leds[4], HIGH);
       }
-      selectedLED2 = getNextLEDPairs(selectedLED2);
+      selectedLED = getNextLEDPairs(selectedLED);
+      
     } else {
+      // Animation 4: Flashing LEDs
       showStarImage(3);
       if (isFlashing) {
         for (int i = 0; i < 5; i++) {
@@ -98,10 +104,29 @@ void loop() {
       }
       isFlashing = !isFlashing;
     }
+    
   }
-
 }
 
+// Helper Functions
+
+// shows the star graphic indicating animation number
+void showStarImage(int animation) {
+  display.clear();
+  if (animation == 0) {
+    display.drawXbm(30, 2, Star_1_width, Star_1_height, Star_1_image);
+  } else if (animation == 1) {
+    display.drawXbm(30, 2, Star_2_width, Star_2_height, Star_2_image);
+  } else if (animation == 2) {
+    display.drawXbm(30, 2, Star_3_width, Star_3_height, Star_3_image);
+  } else {
+    display.drawXbm(30, 2, Star_4_width, Star_4_height, Star_4_image);
+  }
+  
+  display.display();
+}
+
+// gets previous LED in sequence
 int getPrevLED(int val) {
   if (val == 0) {
     return 4;
@@ -110,6 +135,7 @@ int getPrevLED(int val) {
   }
 }
 
+// gets next LED in sequence
 int getNextLED(int val) {
   if (val == 4) {
     return  0;
@@ -118,21 +144,7 @@ int getNextLED(int val) {
   }
 }
 
-void showStarImage(int val) {
-  display.clear();
-  if (val == 0) {
-    display.drawXbm(15, 0, Star_1_width, Star_1_height, Star_1_image);
-  } else if (val == 1) {
-    display.drawXbm(15, 0, Star_2_width, Star_2_height, Star_2_image);
-  } else if (val == 2) {
-    display.drawXbm(15, 0, Star_3_width, Star_3_height, Star_3_image);
-  } else {
-    display.drawXbm(15, 0, Star_4_width, Star_4_height, Star_4_image);
-  }
-  
-  display.display();
-}
-
+// gets next LED pairs in sequence
 int getNextLEDPairs(int val) {
   if (val == 2) {
     return 0;
@@ -141,6 +153,7 @@ int getNextLEDPairs(int val) {
   }
 }
 
+// turns off all LEDs
 void resetLEDs() {
   for (int i = 0; i < 5; i++) {
     digitalWrite(leds[i], LOW);
